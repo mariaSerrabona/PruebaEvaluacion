7b67ff087cab78a46536121613daf472a37b12d3
alexbalduz <83299400+alexbalduz@users.noreply.github.com>
2022-10-07 18:43:32 +0200

    ejercicio 32

---
+++ gestor/ejercicio3/ejercicio32.py
@@ -0,0 +1,19 @@
+# 3.2.1 Crea un array de tamaño (3x4) de todos los números enteros entre 0 y 100 que sean múltiplos de 9.
+import numpy as np
+import random
+
+array_aleatorio=np.random.randint(1, 100, size=(3, 4))
+array = array_aleatorio[array_aleatorio % 9 == 0]
+print(array)
+
+# 3.2.2 Filtra todos los valores del vector creado en el paso anterior de la siguiente manera:
+# Si son valores positivos y múltiplos de 5 se conservan sin cambios
+
+# En el caso contratio se sustituyen con el valor -1
+
+for i in array:
+    if array[i] > 0 & array[i] % 5 == 0:
+        print(array[i])
+    else:
+        array[i] = -1
+
