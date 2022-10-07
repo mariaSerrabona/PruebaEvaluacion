37cc53ccbdf35e70db7ac49ab7c9530e5c503583
alexbalduz <83299400+alexbalduz@users.noreply.github.com>
2022-10-07 18:09:54 +0200

    Ejercicio 2.1

---
+++ gestor/ejercicio_2.1.py
@@ -0,0 +1,19 @@
+#2.1.1 Recorre el listado del ejemplo y realiza las siguientes operaciones: [18, 50, 210, 80, 145, 333, 70, 30]
+# Imprimr el número en caso de que sea múltiplo de 10 y menor que 200
+
+# Parar el programa si llega a un número mayor que 300
+def listado(lista):
+    for numero in lista:
+        while numero < 300:
+            if numero % 10 == 0 & numero < 200:
+                print(numero)
+
+
+
+if __name__ == "__main__":
+    lista = [18, 50, 210, 80, 145, 333, 70, 30]
+    listado(lista)
+
+
+
+
