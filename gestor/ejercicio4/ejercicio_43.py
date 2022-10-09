431d403956f94047e920def14fb0a3360f1e0003
mariaSerrabona <83339874+mariaSerrabona@users.noreply.github.com>
2022-10-09 17:48:08 +0200

    creando el menu

--- gestor/ejercicio4/ejercicio_43.py
+++ gestor/ejercicio4/ejercicio_43.py
@@ -6,46 +6,61 @@ autos=pd.read_csv('/Users/mariaperez-serrabona/PruebaEvaluacion/gestor/ejercicio
            'Weight', 'Acceleration', 'Model Year', 'Origin', 'Car Name'])
 
 
-print(autos.info())
+#print(autos.info())
 
 #4.3.1 ¿Qué coche pesa más de 5000 kg?
 
 autos_mayores_5000 = autos[autos['Weight'] > 5000]
-print(autos_mayores_5000['Car Name'])
+#print(autos_mayores_5000['Car Name'])
 
 #4.3.2 ¿Cuál es el número de cilindros más frecuente en los coches que son del año 76?
 autos_76=autos[autos['Model Year']==76]
-print(autos_76['Cylinders'].mode())
+#print(autos_76['Cylinders'].mode())
 
 #4.3.3 Compara el consumo medio de los coches fabricados en el año 70 con los del año 79 y calcula la diferencia.
 
 autos_70=autos[autos['Model Year']==70]
 autos_79=autos[autos['Model Year']==79]
 
-print(autos_70['MPG'].mean())
-print(autos_79['MPG'].mean())
+#print(autos_70['MPG'].mean())
+#print(autos_79['MPG'].mean())
 
 diferencia=abs(autos_70['MPG'].mean()-autos_79['MPG'].mean())
 
-print(diferencia)
+#print(diferencia)
 
 
 #4.3.4 Identifica las variables que contienen valores nulos (missing values) en caso que haya alguno.
 
-print(autos.columns[autos.isnull().any()])
+#print(autos.columns[autos.isnull().any()])
 
 #4.3.5 Consigue un DataFrame de aquellos registros que tengan algún valor nulo (NaN), filtrando el DataFrame original.
 
 null_columns=autos.columns[autos.isnull().any()]
 autos[null_columns].isnull().sum()
 autos2 = pd.DataFrame(autos[autos.isnull().any(axis=1)])
-print(autos2)
+#print(autos2)
 
 #4.3.6 Calcula la frecuencia del número de cilindros entre los registros del DataFrame anterior (la tabla con valores perdidos).
 
-print(autos2['Cylinders'].mode())
+#print(autos2['Cylinders'].mode())
 
 
 #4.3.7 Crea un DataFrame de los modelos con 6 o más cilindros, que pesén más de 4000 kg y consumen menos o igual que 10 galón por milla, filtrando el dataset original.
 autos_filtrado=autos[(autos['Cylinders']>=6) & (autos['Weight']>4000) & (autos['MPG']<=10)]
-print(autos_filtrado)
+#print(autos_filtrado)
+
+
+def get_datos43():
+    print('El coche que pesa más de 5000 kg es: '+ autos_mayores_5000['Car Name'])
+    print('El número de cilindros más frecuente en los coches que son del año 76 es: '+str(autos_76['Cylinders'].mode()))
+    print('consumo medio de los coches fabricados en 1970: '+str(autos_70['MPG'].mean()))
+    print('consumo medio de los coches fabricados en 1979: '+str(autos_79['MPG'].mean()))
+    print('Diferencia entre ellos: '+str(diferencia))
+    print('Variables con valores nulos: '+autos.columns[autos.isnull().any()])
+    print('DataFrame de aquellos registros que tengan algún valor nulo (NaN):')
+    print(autos2)
+    print('La frecuencia del número de cilindros entre los registros del DataFrame anterior'+ str(autos2['Cylinders'].mode()))
+    print('Conjunto de coches con 6 o más cilindros, que pesén más de 4000 kg y consumen menos o igual que 10 galón por milla:')
+    print(autos_filtrado)
+
--- gestor/menu.py
+++ gestor/menu.py
@@ -1,7 +1,6 @@
 import os
-from ejercicio4.ejercicio4_2 import *
-
-
+from ejercicio4 import ejercicio_43
+from ejercicio3 import ejercicio31
 
 def iniciar():
     while True:
@@ -10,24 +9,46 @@ def iniciar():
         print(" BIENVENIDO AL Examen ")
         print("========================")
         print("[1] Ejercicio 1 ")
-        print("[2] Ejercicio 1 ")
-        print("[3] Ejercicio 1 ")
-        print("[4] Ejercicio 1 ")
+        print("[2] Ejercicio 2 ")
+        print("[3] Ejercicio 3 ")
+        print("[4] Ejercicio 4 ")
         print("[5] Salir")
         print("========================")
         opcion = input("> ")
         os.system('clear') # cls en Windows
 
         if opcion == '1':
-            print(get_estadisticas())
+            pass
         elif opcion == '2':
             pass
         elif opcion == '3':
-            pass
+            print("========================")
+            print(" BIENVENIDO AL EJERCICIO 3 ")
+            print("========================")
+            print("[1] Ejercicio 3.1 ")
+            print("[2] Ejercicio 3.2 ")
+            opcion = input("> ")
+            if opcion=='3.2':
+                ejercicio31.get_information_31()
+
         elif opcion == '4':
-            pass
+            print("========================")
+            print(" BIENVENIDO AL EJERCICIO 4 ")
+            print("========================")
+            print("[1] Ejercicio 4.1 ")
+            print("[2] Ejercicio 4.2 ")
+            print("[3] Ejercicio 4.3 ")
+            print("[4] Ejercicio 4.4 ")
+            opcion = input("> ")
+            if opcion=='4.3':
+                ejercicio_43.get_datos43()
+
         elif opcion == '6':
             print("Saliendo...\n")
             break
 
         input("\nPresiona ENTER para continuar...")
+
+
+iniciar()
+
