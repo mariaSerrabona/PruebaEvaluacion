13ce1e527bcb39a707ae82d82561eb0e448f4563
javiipzo <javipozo999@gmail.com>
2022-10-09 14:12:15 +0200

    ej4.2 excepto grafico

--- gestor/ejercicio4/ejercicio4_2.py
+++ gestor/ejercicio4/ejercicio4_2.py
@@ -1,3 +1,4 @@
+from array import array
 from dataclasses import dataclass
 import pandas as pd
 import seaborn as sns 
@@ -10,39 +11,49 @@ def convert_array(array):
     conversor=lambda x: pd.Series(x)
     return conversor(array)
 #4.2.2
-arr=[10, True, 8.00, False, 8, 10]
-data=convert_array(arr)
+def convertir_datos():
+    arr=[10, True, 8.00, False, 8, 10]
+    data=convert_array(arr)
+    return data
+data=convertir_datos()
 #4.2.3
-#VALORES DE LA SERIE
-print(data.values)
-#INDICES
+def obtener_datos_serie():
+    #VALORES DE LA SERIE
+    #print(data.values)
+    #INDICES
 
-#TIPO DE DATOS
-print(data.dtypes)
-#NUMERO DE BITS EN MEMORIA
-print(data.values.itemsize)
-
-#4.2.4
-
+    #TIPO DE DATOS
+    #print(data.dtypes)
+    #NUMERO DE BITS EN MEMORIA
+    #print(data.values.itemsize)
+    return data.values, data.dtypes,data.values.itemsize
+#4.2.4 GRAFICO
+print(obtener_datos_serie())
 #4.2.5
-print(data.describe())
+def get_estadisticas():
+    return data.describe()
 #4.2.6
-dic={}
-for i in data:
-    if i in dic:
-        dic[i]+=1
-    else:
-        dic[i]=1
-print(dic)
-#4.2.7
-for i in dic:
-    if dic[i]==1:
-        print (i)
-print()
-#4.2.8
+def sacar_frecuencia_y_val_unicos():
+    dic={}
+    for i in data:
+        if i in dic:
+            dic[i]+=1
+        else:
+            dic[i]=1
+    #4.2.7
+    lista=[]
+    for i in dic:
+        if dic[i]==1:
+            lista.append(i)
+    return dic, lista
 
-print(data.sort_values(ascending=False).reset_index(drop=True))
+#4.2.8
+def reordenar_reset():
+    #print(data.sort_values(ascending=False).reset_index(drop=True))
+    return data.sort_values(ascending=False).reset_index(drop=True)
 #4.2.9
-dataej_42 = pd.DataFrame(data)
-dataej_42.columns=['Ej_42']
-print(dataej_42)
+def get_df():
+    dataej_42 = pd.DataFrame(data)
+    dataej_42.columns=['Ej_42']
+    #print(dataej_42)
+    return dataej_42
